beb99543f268ba5038f1a1bf5cac3cf21adf1484
igachev <ivophoenix1135@gmail.com>
2023-11-09 23:02:55 +0200

    added basic html for register page

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -2,7 +2,34 @@
 function Register(props) {
     return (
         <div>
-            <h1>Register</h1>
+            <h1>Register Page</h1>
+
+        <div>
+            <form>
+
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" />
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" />
+        </div>
+
+        <div>
+            <label for="repeatPassword">Repeat Password</label>
+            <input type="password" name="repeatPassword" id="repeatPassword" />
+        </div>
+
+        <div>
+            <button type="submit">Register</button>
+        </div>
+
+            </form>
+
+        </div>
+
         </div>
     )
 }
