c8a7b91ea940da0336ee4dfca4673d677e32489f
igachev <ivophoenix1135@gmail.com>
2023-11-11 17:05:09 +0200

    update Watches page

--- client/src/pages/Watches/Watches.js
+++ client/src/pages/Watches/Watches.js
@@ -1,10 +1,33 @@
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
+import { getAllWatchesAction } from "../../store/actions/watchActions"
+import { useEffect } from "react"
 
 function Watches(props) {
+
+    useEffect(() => {
+    props.getAllWatchesAction() 
+    },[])
+
     return (
         <div>
             <h1>Watches</h1>
+            {props.watches.length > 0
+            ? (props.watches.map((watch) => 
+            <div key={watch._id}>{watch.brand}</div>))
+            : (<p>No watches available</p>)}
         </div>
     )
 }
 
-export default Watches
+const mapStateToProps = (state) => {
+    return {
+        watches: state.watches.watches
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({getAllWatchesAction},dispatch)
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Watches)
