b5c371ecc21fbb100ae9a7c079706cf767fcfeca
igachev <ivophoenix1135@gmail.com>
2023-11-12 17:25:12 +0200

    added createWatch service

--- client/src/services/watchService.js
+++ client/src/services/watchService.js
@@ -8,3 +8,14 @@ export function getAllWatches() {
 export function getWatch(watchId) {
     return axiosInstance.get(`/watches/${watchId}`)
 }
+
+export function createWatch(brand,model,imageLink,battery,mechanism,
+    price,strap,glass,waterResistance) {
+
+const watchData = {
+    brand,model,imageLink,battery,mechanism,
+    price,strap,glass,waterResistance
+}
+
+    return axiosInstance.post(`/watches/create`,watchData)
+}
