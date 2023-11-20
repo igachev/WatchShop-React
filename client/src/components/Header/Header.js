b0576f909fe650d8d74ddb11fc17069e72a549f8
igachev <ivophoenix1135@gmail.com>
2023-11-20 23:11:20 +0200

    added header css

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom"
 import { isAdmin, isAuthenticated } from "../../store/selectors/authSelectors"
 import { connect, useDispatch } from "react-redux"
 import { logoutAction } from "../../store/actions/authActions"
+import "./Header.css"
 
 export function Header(props) {
 
@@ -14,42 +15,47 @@ function onLogout(e) {
 }
 
 return (
-    <div>
+    
         
     <div className="container">
 
+    <div className="img-logo">
+    <Link to="/"><img src="https://images.unsplash.com/photo-1575203091586-611fe505bb0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="logo" /></Link>
+    </div>
+
     <div className="link-container">
-    <Link to='/'>Watches</Link>
-    <Link to='/watches/search'>Search</Link>
+        <ul>
+    <li><Link to='/'>Watches</Link></li>
+    <li><Link to='/watches/search'>Search</Link></li>
     
     {!props.isAuthenticated && (
         <>
-    <Link to='/users/login'>Login</Link>
-    <Link to='/users/register'>Register</Link>
+    <li><Link to='/users/login'>Login</Link></li>
+    <li><Link to='/users/register'>Register</Link></li>
         </>
     )}
 
     {props.isOwner && (
         <>
-        <Link to='/watches/create'>Create Product</Link>
-        <Link to='/users/adminHistory'>All Purchases</Link>
-        <button onClick={onLogout}>Logout</button>
+        <li><Link to='/watches/create'>Create Product</Link></li>
+        <li><Link to='/users/adminHistory'>All Purchases</Link></li>
+        <li><button onClick={onLogout}>Logout</button></li>
         </>
     )}
 
     {props.isAuthenticated && !props.isOwner && (
         <>
-        <Link to='/users/cart'>Cart</Link>
-        <Link to='/users/purchaseHistory'>Purchase History</Link>
-        <button onClick={onLogout}>Logout</button>
+        <li><Link to='/users/cart'>Cart</Link></li>
+        <li><Link to='/users/purchaseHistory'>Purchase History</Link></li>
+        <li><button onClick={onLogout}>Logout</button></li>
         </>
     )}
-
+    </ul>
     </div>
 
     </div>
 
-    </div>
+    
 )
 }
 
